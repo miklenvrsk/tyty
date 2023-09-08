6b306dc0be7bcaa179af6e3cf2f24c3300e52c9a
Михаил Скворцов <miklenvrsk5@yandex.ru>
2023-09-08 10:42:03 +0300

    mob version

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+   
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
